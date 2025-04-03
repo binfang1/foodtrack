ba1642747354a90df7bfb030a0f067ffc87a1a07
RickyLiTan <151973832+RicLiTan@users.noreply.github.com>
2025-04-03 13:49:02 -0600

    Styling Changes

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -40,13 +40,12 @@ export default function Login({loggedIn, setLoggedIn, accounts, page, setPage}) 
         <div className="w-[100vw] h-[100vh] flex">
             <div className="w-[25vw] h-[30vw] p-[2vw] bg-white drop-shadow-md rounded-xl border-solid border-3 border-[#D9D9D9] m-auto">
                 <h1 className="text-[1.5vw] text-left mb-[1vw]">FoodTrack</h1>
-                <form onSubmit={checkLogin} className="flex flex-col m-auto">
-                    <h1 className="text-[2vw] text-left mb-[1vw]">Login</h1>
-                    <label className="text-[1.5vw]">Username:
-                        <input placeholder="Username" onChange={getUser} className = "mb-[1vw] w-full border-gray-500 border-2 pl-[0.1vw] pr-[0.1vw] text-black" type="text" required/>
+                <form onSubmit={checkLogin} className="flex flex-col m-auto gap-[2vw] mt-[5vw]">
+                    <label className="text-[1.5vw]">
+                        <input placeholder="Username" onChange={getUser} className = "rounded-md mb-[1vw] w-full h-[3vw]  border-gray-500 border-2 pl-[1vw] pr-[0.1vw] text-black" type="text" required/>
                     </label>
-                    <label className="text-[1.5vw]">Password:
-                        <input placeholder="password" onChange={getPass} className = "mb-[3vw] w-full border-gray-500 border-2 pl-[0.1vw] pr-[0.1vw] text-black" type="password" required/>
+                    <label className="text-[1.5vw]">
+                        <input placeholder="Password" onChange={getPass} className = "rounded-md mb-[1vw] w-full h-[3vw] border-gray-500 border-2 pl-[1vw] pr-[0.1vw] text-black" type="password" required/>
                     </label>
                     <input className="cursor-pointer w-full h-[3vw] text-[1.5vw] bg-white drop-shadow-md rounded-xl border-solid border-3 border-[#D9D9D9] text-black rounded-lg shadow-md  m-auto" type = "submit" value = "Login"/>
                 </form>
